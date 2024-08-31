cf0d24493bda06d2cf8fe0b77381ca89efb43b0e
4arnab <axmedarnab3@gmail.com>
2024-08-31 12:43:14 +0530

    final tech

--- index.php
+++ index.php
@@ -6,11 +6,10 @@ require_once "./account.php";
 $userOne = new Account("Ahmed Abdifatah", 101);
 
 // User Transactions
-$userOne->deposit('da');
-// $userOne->deposit(2333);
-// $userOne->withdraw(450);
+$userOne->deposit('1235');
+$userOne->deposit(2333);
+$userOne->withdraw(450);
 
-// $userOne->getAllTransactions();
-// echo json_encode($userOne->transactions);
+$userOne->getAllTransactions();
 
 $userOne = new Account("Abdirahman Mohamed", 102);
