04bc61ca59e4e7eea2c395470f71d16710576f7c
4arnab <axmedarnab3@gmail.com>
2024-08-31 12:44:03 +0530

    fincal touches

--- index.php
+++ index.php
@@ -6,7 +6,7 @@ require_once "./account.php";
 $userOne = new Account("Ahmed Abdifatah", 101);
 
 // User Transactions
-$userOne->deposit('1235');
+$userOne->deposit(1235);
 $userOne->deposit(2333);
 $userOne->withdraw(450);
 
